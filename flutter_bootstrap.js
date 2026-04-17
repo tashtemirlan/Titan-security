c43a85e1c162addfb214c38b3bd7765d0b806d67
Temirlan Tashtanov <tashtanov.temirlan@gmail.com>
2026-04-17 23:43:03 +0600

    FEAT : changed icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2270590040" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2159944786" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
